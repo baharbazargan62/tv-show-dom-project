f14f41d3ffd8743c8a4868a87cc46df6b8e8a552
baharbazargan62 <baharbazargan62@gmail.com>
2022-11-01 14:40:30 +0000

    adding codeBar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//You can edit ALL of the code here
+// You can edit ALL of the code here
 function setup() {
-  const allEpisodes =getAllEpisodes();
+  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 function makePageForEpisodes(episodeList) {
@@ -9,25 +9,31 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
-window.onload=()=>{
-  const divEl=document.getElementById("root")
-  const sectionEl=document.getElementById("main-page");
-  const episodes=getAllEpisodes()
+window.onload = () => {
+  const divEl = document.getElementById("root");
+  const sectionEl = document.getElementById("main-page");
+  const episodes = getAllEpisodes();
   divEl.appendChild(sectionEl);
-for (let episode of episodes){
-const articleEl=document.createElement("article");
-const h3=document.createElement("h3");
-const image=document.createElement("img");
-let pragragh=document.createElement("p")
-sectionEl.appendChild(articleEl);
-articleEl.appendChild(h3);
-articleEl.appendChild(image);
-articleEl.appendChild(pragragh);
-h3.innerText=episode.name;
-image.src=episode.image.medium;
-pragragh.innerHTML=episode.summary
+   for(let episode of episodes) {
+    const articleEl = document.createElement("article");
+    const h3 = document.createElement("h3");
+    const image = document.createElement("img");
+    let pragragh = document.createElement("p");
+    sectionEl.appendChild(articleEl);
+    articleEl.appendChild(h3);
+    articleEl.appendChild(image);
+    articleEl.appendChild(pragragh);
+    h3.innerText = episode.name+"-"+episode.season.toString().padStart(3,"S0")+episode.number.toString().padStart(3,"E0")
+    image.src = episode.image.medium;
+    pragragh.innerHTML = episode.summary;
+  }
+  let footer=document.createElement("footer")
+  let link=document.createElement("a")
+  link.setAttribute("href","https://www.tvmaze.com/")
+  link.innerText="TVMaze.com"
+  divEl.appendChild(footer)
+  footer.appendChild(link)
 }
-  
 
 
-}
+
