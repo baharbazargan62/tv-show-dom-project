a4ed5ed1d143c6162c635be8202f0f0d28bc2c26
baharbazargan62 <baharbazargan62@gmail.com>
2022-11-03 11:13:08 +0000

    complete step 200

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
-
-window.onload = setup;
-window.onload = () => {
   const divEl = document.getElementById("root");
   const sectionEl = document.getElementById("main-page");
-  const episodes = getAllEpisodes();
   divEl.appendChild(sectionEl);
-   for(let episode of episodes) {
+  //search bar
+  const selectEl=document.querySelector("#select-option");
+  const searchInput=document.querySelector("#search-input")
+   for(let episode of episodeList) {
+    const optionEl=document.createElement("option");
+    selectEl.appendChild(optionEl);
+    optionEl.innerText=episode.season.toString().padStart(3,"S0")+episode.number.toString().padStart(3,"E0")+"-"+episode.name
     const articleEl = document.createElement("article");
+    articleEl.setAttribute("class","each-film")
     const h3 = document.createElement("h3");
+    h3.setAttribute("class","header")
     const image = document.createElement("img");
     let pragragh = document.createElement("p");
     sectionEl.appendChild(articleEl);
@@ -26,14 +27,25 @@ window.onload = () => {
     h3.innerText = episode.name+"-"+episode.season.toString().padStart(3,"S0")+episode.number.toString().padStart(3,"E0")
     image.src = episode.image.medium;
     pragragh.innerHTML = episode.summary;
-  }
+     }
+     let spanEl=document.getElementById("count-epispde");
+     searchInput.addEventListener('input',checkMatchEpisode)
+     function checkMatchEpisode(){
+       let matchList=episodeList.filter(episode=>{return (episode.name.toLowerCase().includes(searchInput.value.toLowerCase())||episode.summary.toLowerCase().includes(searchInput.value.toLowerCase()))
+       })
+       spanEl.innerText=matchList.length +' / '+ episodeList.length
+     }
   let footer=document.createElement("footer")
   let link=document.createElement("a")
   link.setAttribute("href","https://www.tvmaze.com/")
   link.innerText="TVMaze.com"
   divEl.appendChild(footer)
   footer.appendChild(link)
-}
+};
+
+window.onload = setup;
+
+
 
 
 
